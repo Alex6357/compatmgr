00af63c9bfe6916aeb3df47ee9403fb41b41d0b1
Alex6357 <73743056+Alex6357@users.noreply.github.com>
2023-08-14 22:02:17 +0800

    Enable fetch gentoo.sh

--- scripts/fetch/gentoo.sh
+++ scripts/fetch/gentoo.sh
@@ -80,28 +80,28 @@ if [ ! -d ${DIR}/temp ]; then
     mkdir ${DIR}/temp
 fi
 
-# fetch ${URL} -o ${DIR}/temp/${FILE}
-# STATUS=${?}
-# if [ ${STATUS} -ne 0 ]; then
-#     export STATUS
-#     echo $(tr INSTALL_FAILED_FETCH)
-#     exit 2
-# fi
+fetch ${URL} -o ${DIR}/temp/${FILE}
+STATUS=${?}
+if [ ${STATUS} -ne 0 ]; then
+    export STATUS
+    echo $(tr INSTALL_FAILED_FETCH)
+    exit 2
+fi
 
-# tar xvpf ${DIR}/temp/${FILE} -C ${INSTALL_DIR} --numeric-owner
-# STATUS=${?}
-# if [ ${STATUS} -ne 0 ]; then
-#     export STATUS
-#     echo $(tr INSTALL_FAILED_TAR)
-#     exit 3
-# else
-#     echo $(tr INSTALL_COMPLETE)
-#     echo ""
-#     echo $(tr SETTING_UP)
-#     echo ""
-#     ${DIR}/scripts/setup/gentoo/setup.sh
-#     rm -rf ${DIR}/temp
-# fi
+tar xvpf ${DIR}/temp/${FILE} -C ${INSTALL_DIR} --numeric-owner
+STATUS=${?}
+if [ ${STATUS} -ne 0 ]; then
+    export STATUS
+    echo $(tr INSTALL_FAILED_TAR)
+    exit 3
+else
+    echo $(tr INSTALL_COMPLETE)
+    echo ""
+    echo $(tr SETTING_UP)
+    echo ""
+    ${DIR}/scripts/setup/gentoo/setup.sh
+    rm -rf ${DIR}/temp
+fi
 
 ${DIR}/scripts/setup/gentoo/setup.sh
 exit 9
